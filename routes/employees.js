83251bdf4595b361e8e2fee03c3e76a01ec7bbb2
nathanguevara <nathan.guevara@gmail.com>
2022-06-30 19:55:29 -0500

    fixed routes

--- routes/employees.js
+++ routes/employees.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const usersController = require("../controllers/employees");
+const employeesController = require("../controllers/employees");
 const router = express.Router();
 
-router.get("/employees", usersController.getEmployees);
+router.get("/", employeesController.getEmployees);
 
-router.get("/:employee_id", usersController.getEmployeesById);
+router.get("/:employee_id", employeesController.getEmployeesById);
 
-router.get("/:first_name", usersController.getEmployeesByFirstName);
+router.get("/:first_name", employeesController.getEmployeesByFirstName);
 
 module.exports = router;
