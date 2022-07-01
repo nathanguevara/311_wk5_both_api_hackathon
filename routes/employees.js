56e3c7b8cf6db7d1b8d0f4d77f1d41ad68f0013c
eradoria <eradoria@gmail.com>
2022-06-30 19:28:08 -0500

    4

--- routes/employees.js
+++ routes/employees.js
@@ -2,10 +2,10 @@ const express = require("express");
 const usersController = require("../controllers/employees");
 const router = express.Router();
 
-router.get("/employees", usersController.getAllUsers);
+router.get("/employees", usersController.getEmployees);
 
-router.get("/:employee_id", usersController.getUserById);
+router.get("/:employee_id", usersController.getEmployeesById);
 
-router.get("/:first_name", usersController.getUserByFirstName);
+router.get("/:first_name", usersController.getEmployeesByFirstName);
 
 module.exports = router;
