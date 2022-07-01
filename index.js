6a85c9e8b88b5235782a6f3f67753400e7c06574
eradoria <eradoria@gmail.com>
2022-06-30 19:20:48 -0500

    3

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4001
+const express = require("express");
+const usersRouter = require("./routes/employees");
+const app = express();
+const port = process.env.PORT || 4001;
 
-app.get('/', (req, res) => res.send('Welcome to our API'))
+app.get("/", (req, res) => res.send("Welcome to our API"));
+
+app.use(express.json());
+app.use("/employees", usersRouter);
 
 app.listen(port, () => {
-    console.log('app is listening on:', port)
-  })
+  console.log("app is listening on:", port);
+});
