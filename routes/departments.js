61bf8ebd411c6723cdf700568bc8a6c6014058d0
eradoria <eradoria@gmail.com>
2022-06-30 20:29:26 -0500

    7

--- routes/departments.js
+++ routes/departments.js
@@ -2,10 +2,10 @@ const express = require("express");
 const departmetsController = require("../controllers/departments");
 const router = express.Router();
 
-router.get("/", departmetsController.getDepartemnts);
+router.get("/", departmetsController.getDepartments);
 
-router.get("/:id", departmetsController.getDepartemntsId);
+router.get("/:id", departmetsController.getDepartmentsId);
 
-router.get("/:department_name", departmetsController.getDepartemntsByName);
+router.get("/:department_name", departmetsController.getDepartmentsByName);
 
 module.exports = router;
