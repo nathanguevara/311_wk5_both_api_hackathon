d8437e23ee610439da8ebcbe684393a2726544b7
eradoria <eradoria@gmail.com>
2022-06-30 19:15:56 -0500

    2

--- routes/employees.js
+++ routes/employees.js
@@ -2,7 +2,7 @@ const express = require("express");
 const usersController = require("../controllers/employees");
 const router = express.Router();
 
-router.get("/", usersController.getAllUsers);
+router.get("/employees", usersController.getAllUsers);
 
 router.get("/:employee_id", usersController.getUserById);
 
