e85e9d5e8738c8a405ddc7c9eec4dc77e16e3e9e
JTDoe <Jordendoerflinger@gmail.com>
2022-06-30 20:25:17 -0500

    4th

---
+++ controllers/addresses.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql");
+const pool = require("../mysql/connection.js");
+const { handleSQLError } = require("../mysql/error.js");
+
+const getAddress = (req, res) => {
+  pool.query("SELECT * FROM addresses", (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const getStreetName = (req, res) => {
+  let id = req.params.street;
+  let sql = "SELECT * FROM ?? WHERE ?? = ?";
+
+  sql = mysql.format(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const getZip = (req, res) => {
+  let firstName = req.params.zipcode;
+  let sql = "SELECT * FROM ?? WHERE ?? = ?";
+
+  sql = mysql.format(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+module.exports = { getAddress, getStreetName, getZip };
