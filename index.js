66c218b707b42a4c8358a97e23c1b00181db194f
eradoria <eradoria@gmail.com>
2022-07-05 20:24:43 -0500

    11

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get("/", (req, res) => res.send("Welcome to our API"));
 app.use(express.json());
 app.use("/employees", usersRouter);
 
-
 app.listen(port, () => {
   console.log("app is listening on:", port);
 });
--- routes/address.js
+++ routes/address.js
@@ -2,19 +2,10 @@ const express = require("express");
 const addressesController = require("../controllers/addresses");
 const router = express.Router();
 
-
-router.get("/", addressesController.getZip);
-
-router.get("/:id", addressesController.getUserById);
-
-router.get("/:first_name", addressesController.getUserByFirstName);
-
 router.get("/", employeesController.getAddress);
 
-router.get("/:street", employeesController.getStreetName);
+router.get("/:id", employeesController.getStreetName);
 
 router.get("/:first_name", employeesController.getZip);
-
-router.get("/:zip", employeesController.getZip);
 
 module.exports = router;
